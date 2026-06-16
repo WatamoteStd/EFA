814f7b2d2dc16af6a26ce7425eb66a5dbf01f356
watashiwa <watashiwadesu121@gmail.com>
2026-06-16 12:47:40 +0300

    EFA v0.0.1: Working text parser.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EFA.Core;
+
+EFACore core = new EFACore();
+
+Console.WriteLine();
+Console.WriteLine("EFA: v0.0.1");
+Console.WriteLine("========================================================================");
+
+while(true)
+{
+    
+    string? userInput = Console.ReadLine();
+    string? coreResponse = core.ProcessInput(userInput);
+    if (!string.IsNullOrEmpty(coreResponse))
+    {
+        Console.WriteLine($"Request: {coreResponse}");
+
+    }
+    
+
+}
